8d1629cb5c243263f3d2057763b40216e0d162b5
BogdanP <67414934+anomancod@users.noreply.github.com>
2023-03-10 10:19:42 +0300

    GREAT KNOWLEDGE ADDED FOR THE SECOND TIME!
    your karma's influence has been reduced.

--- Hangman.py
+++ Hangman.py
@@ -1,8 +1,10 @@
 import random
-words = ['аббревиатура', 'абстрагирование', 'валидность', 'гармонизация', 'дезорганизация', 'детерминизм', 'группировка', 'диссертабельность', 'дублирование', 'заблуждение', 'закономерность', 'идентификация', 'издательство']
+words = ['аббревиатура', 'абстрагирование', 'валидность', 'гармонизация', 'дезорганизация', 'детерминизм', 'группировка', 'диссертабельность', 'дублирование', 'заблуждение', 'закономерность', 'идентификация', 'издательство', 'экосистема']
 f = 0
+b = 0
 while True:
     word = random.choice(words)
+    wor = word
     w = '*' * len(word)
     print(w)
     while True:
@@ -19,26 +21,22 @@ while True:
                 print('Вы ввели неверное слово.')
         elif let in word and let.isalpha():
             f += 1
-            x = word.find(let)
-            x1 = word.rfind(let)
-            if x == x1:
-                y = w[x + 1:]
-                y1 = w[:x]
-                w1 = y1 + let + y
-                w = w1
-                print(w)
-            else:
-                y = w[x + 1:]
-                y1 = w[:x]
-                w1 = y1 + let + y
-                w = w1
+            for i in range(len(word)):
+                if word[i:i + 1] == let:
+                    b += 1
 
-                y = w[x1 + 1:]
-                y1 = w[:x1]
-                w1 = y1 + let + y
-                w = w1
+            for i in range(b):
+                x = wor.find(let)
 
-                print(w)
+                wor1 = wor[:x]
+                wor2 = wor[x + 1:]
+                wor = wor1 + '*' + wor2
+
+                w1 = w[:x]
+                w2 = w[x + 1:]
+                w = w1 + let + w2
+            print(w)
+            b = 0
         else:
             print('Вы ввели что-то не то.')
         if w == word and let.isalpha():
